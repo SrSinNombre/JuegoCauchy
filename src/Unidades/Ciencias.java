cd659baefcb909a1c35775873f24193727e9a121
adri <adrianruesga@gmail.com>
2025-05-01 12:41:45 +0200

    Unidades actualizacion

--- src/Unidades/Ciencias.java
+++ src/Unidades/Ciencias.java
@@ -1,6 +1,9 @@
 package Unidades;
 
 public class Ciencias {
-    Unidades cientifico = new Unidades(100, 40, 60, 2, 3);
-    Unidades biologo = new Unidades(120,15,40,2,1)  ;
+    Unidades quimico = new Unidades(100, 40, 60, 2, 3);
+    Unidades biologo = new Unidades(120,15,40,2,1);
+    Unidades matematico = new Unidades(150, 35, 50, 3, 3 );
+    Unidades fisico = new Unidades(110, 30, 40, 2, 1);
+    Unidades ingeniero = new Unidades(80, 60, 30, 4, 3);
 }
--- src/Unidades/Letras.java
+++ src/Unidades/Letras.java
@@ -3,4 +3,7 @@ package Unidades;
 public class Letras {
     Unidades historiador = new Unidades(130,35,50,4,1);
     Unidades filologo = new Unidades(70,45,30,2,4);
+    Unidades teologo = new Unidades(120,40, 20, 3, 2);
+    Unidades traductor = new Unidades(110, 25, 40, 3, 2);
+    Unidades filosofo = new Unidades(150, 20, 45, 1, 3);
 }
