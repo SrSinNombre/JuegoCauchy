6c43ebfc5e088a6ce31539a668ea2c8582cac3ba
Faateh Guliyev <faateh.guliyev@edu.uah.es>
2025-05-20 13:57:07 +0200

    adri prueba ahora

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module org.example.juegogui {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
+    requires org.apache.logging.log4j.core;
+    requires org.apache.logging.log4j;
 
 
     opens org.example.juegogui to javafx.fxml;
